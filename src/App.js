b4f370f2252674bbf932601809c36dccdc624196
Nehaa7255 <nehamas04@gmail.com>
2024-05-26 23:52:03 +0530

    Style added.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import data from './data.js'
 import Tour from './components/Tour.js';
 
-
 const App = () => {
   const [tour, setTour]=useState(data)
 
@@ -11,13 +10,10 @@ const App = () => {
     const newTour= tour.filter(tour => tour.id !==id);
     setTour =(newTour);
   }
-
   return (
-  
     <div>
     <Tour tour={tour} removeTour={removeTour}></Tour>
     </div>   
-  
   );
 }
 export default App;
